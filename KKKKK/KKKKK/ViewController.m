fb2b84371b5b84c2dda5d3a19450b4e842d3de4f
gtja <1194554295@qq.com>
2020-12-11 10:43:01 +0800

    测试一下

---
+++ KKKKK/KKKKK/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KKKKK
+//
+//  Created by gtja on 2020/12/11.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(30, 100, 100, 30)];
+    label.text = @"ssssss";
+    label.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:label];
+    label.textColor = [UIColor greenColor];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
